4e4dfb4ab11949171c34ceb450e1c15c5b304109
= <=>
2025-04-14 01:34:50 -0400

    added tmux config

--- .config/nvim/lua/config/plugins/cmp.lua
+++ .config/nvim/lua/config/plugins/cmp.lua
@@ -1,14 +1,9 @@
 local M = { -- Autocompletion
     "hrsh7th/nvim-cmp",
-    -- event = 'InsertEnter',
     dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
         {
             "L3MON4D3/LuaSnip",
             build = (function()
-                -- Build Step is needed for regex support in snippets
-                -- This step is not supported in many windows environments
-                -- Remove the below condition to re-enable on windows
                 if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
                     return
                 end
@@ -16,21 +11,17 @@ local M = { -- Autocompletion
             end)(),
         },
         "saadparwaiz1/cmp_luasnip",
-
-        -- Adds other completion capabilities.
-        --  nvim-cmp does not ship with all sources by default. They are split
-        --  into multiple repos for maintenance purposes.
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
-
-        -- Adds a number of user-friendly snippets
+        "hrsh7th/cmp-cmdline", -- 🔥 Enables : and / completion
         "rafamadriz/friendly-snippets",
     },
     config = function()
         local cmp = require("cmp")
-        require("luasnip.loaders.from_vscode").lazy_load()
         local luasnip = require("luasnip")
+
+        require("luasnip.loaders.from_vscode").lazy_load()
         luasnip.config.setup({})
 
         local kind_icons = {
@@ -61,31 +52,18 @@ local M = { -- Autocompletion
             TypeParameter = "󰊄",
         }
 
+        -- Insert mode completion
         cmp.setup({
             snippet = {
                 expand = function(args)
                     luasnip.lsp_expand(args.body)
                 end,
             },
-            -- completion = { completeopt = 'menu,menuone,noinsert' },
-            -- window = {
-            --     completion = cmp.config.window.bordered(),
-            --     documentation = cmp.config.window.bordered(),
-            -- },
             mapping = cmp.mapping.preset.insert({
-                ["<C-j>"] = cmp.mapping.select_next_item(), -- Select the [n]ext item
-                ["<C-k>"] = cmp.mapping.select_prev_item(), -- Select the [p]revious item
-                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept the completion with Enter.
-                ["<C-c>"] = cmp.mapping.complete({}),  -- Manually trigger a completion from nvim-cmp.
-
-                -- Think of <c-l> as moving to the right of your snippet expansion.
-                --  So if you have a snippet that's like:
-                --  function $name($args)
-                --    $body
-                --  end
-                --
-                -- <c-l> will move you to the right of each of the expansion locations.
-                -- <c-h> is similar, except moving you backwards.
+                ["<C-j>"] = cmp.mapping.select_next_item(),
+                ["<C-k>"] = cmp.mapping.select_prev_item(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-c>"] = cmp.mapping.complete({}),
                 ["<C-l>"] = cmp.mapping(function()
                     if luasnip.expand_or_locally_jumpable() then
                         luasnip.expand_or_jump()
@@ -96,8 +74,6 @@ local M = { -- Autocompletion
                         luasnip.jump(-1)
                     end
                 end, { "i", "s" }),
-
-                -- Select next/previous item with Tab / Shift + Tab
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
@@ -126,9 +102,7 @@ local M = { -- Autocompletion
             formatting = {
                 fields = { "kind", "abbr", "menu" },
                 format = function(entry, vim_item)
-                    -- Kind icons
                     vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-                    -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
                     vim_item.menu = ({
                         nvim_lsp = "[LSP]",
                         luasnip = "[Snippet]",
@@ -137,6 +111,87 @@ local M = { -- Autocompletion
                     })[entry.source.name]
                     return vim_item
                 end,
+            },
+        })
+
+        -- Command-line mode completion (":")
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline({
+                ["<Tab>"] = {
+                    c = function()
+                        cmp.select_next_item()
+                    end,
+                },
+                ["<S-Tab>"] = {
+                    c = function()
+                        cmp.select_prev_item()
+                    end,
+                },
+                ["<Up>"] = {
+                    c = function()
+                        cmp.select_prev_item()
+                    end,
+                },
+                ["<Down>"] = {
+                    c = function()
+                        cmp.select_next_item()
+                    end,
+                },
+                ["<CR>"] = {
+                    c = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    }),
+                },
+            }),
+            sources = cmp.config.sources({
+                { name = "path" },
+                { name = "cmdline" },
+            }),
+            sorting = {
+                comparators = {
+                    cmp.config.compare.sort_text, -- sort by textual similarity
+                    cmp.config.compare.score,
+                    cmp.config.compare.offset,
+                    cmp.config.compare.exact,
+                    cmp.config.compare.length,
+                    cmp.config.compare.order,
+                },
+            },
+        })
+
+        -- Search mode completion ("/")
+        cmp.setup.cmdline("/", {
+            mapping = cmp.mapping.preset.cmdline({
+                ["<Tab>"] = {
+                    c = function()
+                        cmp.select_next_item()
+                    end,
+                },
+                ["<S-Tab>"] = {
+                    c = function()
+                        cmp.select_prev_item()
+                    end,
+                },
+                ["<Up>"] = {
+                    c = function()
+                        cmp.select_prev_item()
+                    end,
+                },
+                ["<Down>"] = {
+                    c = function()
+                        cmp.select_next_item()
+                    end,
+                },
+                ["<CR>"] = {
+                    c = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    }),
+                },
+            }),
+            sources = {
+                { name = "buffer" },
             },
         })
     end,
--- .config/nvim/lua/config/plugins/noice.lua
+++ .config/nvim/lua/config/plugins/noice.lua
@@ -1,92 +1,98 @@
 local M = {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {},
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
-	config = function()
-		require("noice").setup({
-			cmdline = {
-				enabled = true,
-				view = "cmdline_popup", -- VSCode-style floating input
-				format = {
-					cmdline = { icon = "", title = "", lang = "vim" },
-					search_down = { icon = " ", lang = "regex" },
-					search_up = { icon = " ", lang = "regex" },
-				},
-			},
-			messages = {
-				enabled = true,
-				view = "mini", -- or "notify" or "split"
-				view_error = "notify", -- show errors as nvim-notify
-				view_warn = "notify", -- show warnings as nvim-notify
-				view_history = "messages",
-				view_search = "virtualtext",
-			},
-			popupmenu = {
-				enabled = true,
-				backend = "nui", -- replaces native wildmenu
-			},
-			lsp = {
-				progress = {
-					enabled = true,
-					format = " %l %p%%",
-					throttle = 1000 / 30,
-				},
-				signature = {
-					enabled = true,
-					auto_open = {
-						enabled = true,
-						trigger = true,
-						luasnip = true,
-					},
-				},
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-			views = {
-				cmdline_popup = {
-					position = {
-						row = "90%",
-						col = "50%",
-					},
-					size = {
-						width = 60,
-						height = "auto",
-					},
-					border = {
-						style = "rounded",
-						padding = { 0, 1 },
-					},
-					win_options = {
-						winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-					},
-				},
-			},
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						kind = "",
-						find = "written",
-					},
-					opts = { skip = true }, -- suppress "x lines written" message
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = false,
-				lsp_doc_border = false,
-			},
-		})
-	end,
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    config = function()
+        require("noice").setup({
+            cmdline = {
+                enabled = true,
+                -- view = "cmdline_popup", -- VSCode-style floating input
+                view = "cmdline",
+                format = {
+                    cmdline = { icon = "", title = "", lang = "vim" },
+                    search_down = { icon = " ", lang = "regex" },
+                    search_up = { icon = " ", lang = "regex" },
+                },
+            },
+            messages = {
+                enabled = true,
+                view = "mini", -- or "notify" or "split"
+                view_error = "notify", -- show errors as nvim-notify
+                view_warn = "notify", -- show warnings as nvim-notify
+                view_history = "messages",
+                view_search = "virtualtext",
+            },
+            popupmenu = {
+                enabled = true,
+                backend = "nui", -- replaces native wildmenu
+            },
+            lsp = {
+                progress = {
+                    enabled = true,
+                    format = " %l %p%%",
+                    throttle = 1000 / 30,
+                },
+                signature = {
+                    enabled = true,
+                    auto_open = {
+                        enabled = true,
+                        trigger = true,
+                        luasnip = true,
+                    },
+                },
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
+            },
+            -- views = {
+            --     cmdline_popup = {
+            --         keys = {
+            --             ["<Up>"] = "previous",
+            --             ["<Down>"] = "next",
+            --         },
+            --         position = {
+            --             row = "90%",
+            --             col = "50%",
+            --         },
+            --         size = {
+            --             width = 60,
+            --             height = "auto",
+            --         },
+            --         border = {
+            --             style = "rounded",
+            --             padding = { 0, 1 },
+            --         },
+            --         win_options = {
+            --             winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+            --         },
+            --     },
+            -- },
+            routes = {
+                {
+                    filter = {
+                        event = "msg_show",
+                        kind = "",
+                        find = "written",
+                    },
+                    opts = { skip = true }, -- suppress "x lines written" message
+                },
+            },
+            presets = {
+                bottom_search = true,
+                command_palette = false,
+                -- command_palette = true,
+                long_message_to_split = true,
+                inc_rename = false,
+                lsp_doc_border = false,
+            },
+        })
+    end,
 }
 
 return { M }
